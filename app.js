281d690ab2482d7dc2d126c24942c9138abb9835
lucas amorim <lucas amorim>
2016-07-04 10:36:54 -0300

    - Estrutura inicial de projeto MVC criada

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var fs = require('fs');
+var http = require('http');
+var port = 3000;
 
 var app = express();
+
+var routes = require('./controllers/index');
+var users = require('./controllers/users');
+
+// connection data
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : ''
+});
+
+// establish connection to mysql database
+connection.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,34 +41,36 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-
 module.exports = app;
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
--- controllers/users.js
+++ controllers/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/**
+ * About page route
+ */
+router.get('/login', function(req, res) {
+    res.render('index')
+});
+
 module.exports = router;
